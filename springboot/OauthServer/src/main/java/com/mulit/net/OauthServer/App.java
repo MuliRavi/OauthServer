a924affb528619d7f837a6f454dc5f9ce6227446
Ravi-PC\Ravi <ravi.muli@gmail.com>
2020-02-06 13:34:06 +0530

    all dependent oauth files

---
+++ springboot/OauthServer/src/main/java/com/mulit/net/OauthServer/App.java
@@ -0,0 +1,13 @@
+package com.mulit.net.OauthServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
